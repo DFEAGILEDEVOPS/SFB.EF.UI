ddcf0c526ed9414465ff09f9a2ceaa2cbf4bdb19
Serkan Sozer <eserkansozer@hotmail.com>
2020-03-31 16:51:24 +0100

    #40053-API call

---
+++ src/app/Models/EMModel.ts
@@ -0,0 +1,12 @@
+export class EMModel {
+  contextData: ContextData;
+  efficiencyMetricData: EfficiencyMetricData;
+}
+
+export class ContextData {
+  establishmentName: string;
+}
+
+export class EfficiencyMetricData {
+  efficiencydecileingroup: number;
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { EMModel } from 'app/Models/EMModel';
 
 @Component({
   selector: 'app-home',
@@ -9,15 +11,21 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   urn: number;
   schoolName: string;
+  emRank: number;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private http: HttpClient) {
     this.route.params.subscribe(params => {
       this.urn = +params.urn;
     });
   }
 
   ngOnInit() {
-    this.schoolName = 'test';
+    this.http.get('https://aa-t1dv-sfb.azurewebsites.net/api/efficiencymetric/' + this.urn).
+    subscribe(result => {
+      const emModel = result as EMModel;
+      this.schoolName = emModel.contextData.establishmentName;
+      this.emRank = emModel.efficiencyMetricData.efficiencydecileingroup;
+    });
   }
 
 }
